a07dc6a4612e100702c82e15a77430654f8a6c5a
Paul Cutler <paul.r.cutler@gmail.com>
2019-07-10 15:59:19 -0500

    Add additional tests for new pump refactor
    All but one marked pass - need to finish

--- tests/test_pump.py
+++ tests/test_pump.py
@@ -1,13 +1,25 @@
 from gpiozero import *
 
 
-def test_run_pump(mock_factory):
+def test_pump_on(mock_factory):
     pump1 = mock_factory.pin(4)
 
     with OutputDevice(4) as device:
+
         device.on()
         assert pump1.state
         device.off()
 
     assert not pump1.state
 
+
+def test_pump_time()
+    pass
+
+
+def test_pump_stop(mock_factory):
+    pass
+
+
+def test_pump_series():
+    pass
