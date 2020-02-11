6dfbac3d7bdf5c79e49acf600d803d19361ad018
Paul Cutler <paul.r.cutler@gmail.com>
2020-02-11 11:34:18 -0600

    Add step-rpigpio.py
    This uses the RPI.GPIO Python Package, not gpiozero like the rest of this package.  Included from tutorial and need to port
    to gpizero

---
+++ step-rpigpio.py
@@ -0,0 +1,34 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+## This code uses the RPi.GPIO Python Package - not gpiozero like the original pump
+
+DIR = 20   # Direction GPIO Pin
+STEP = 21  # Step GPIO Pin
+CW = 1     # Clockwise Rotation
+CCW = 0    # Counterclockwise Rotation
+SPR = 48   # Steps per Revolution (360 / 7.5)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.output(DIR, CW)
+
+step_count = SPR
+delay = .0208
+
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+sleep(.5)
+GPIO.output(DIR, CCW)
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+GPIO.cleanup()
